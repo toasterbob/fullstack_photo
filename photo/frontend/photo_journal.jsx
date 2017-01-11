c8aaa01b7c7d1ec5d9ab943ad305f4893744b003
Mark Noizumi <marknoizumi@Marks-MacBook-Pro.local>
2017-01-10 19:53:28 -0800

    completed login process, added bootstrapping of current user, created onEnter hooks for after login

--- photo/frontend/photo_journal.jsx
+++ photo/frontend/photo_journal.jsx
@@ -1,31 +1,23 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {signup, login, logout} from './util/session_api_util';
-// import {signup, login, logout} from './actions/session_actions';
+//import {signup, login, logout} from './util/session_api_util';
+import {signup, login, logout} from './actions/session_actions';
 import configureStore from './store/store.js';
-// import Root from './components/root';
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//     let store;
-//     if (window.currentUser) {
-//       const preloadedState = { session: { currentUser: window.currentUser } };
-//       store = configureStore(preloadedState);
-//     } else {
-//       store = configureStore();
-//     }
-//     //const store = configureStore();
-//     const root = document.getElementById('root');
-//     window.store = store;
-//     window.login = login;
-//     window.logout = logout;
-//     ReactDOM.render(<Root store={ store }/>, root);
-// });
+import Root from './components/root';
+
 document.addEventListener('DOMContentLoaded', () => {
+    let store;
+    if (window.currentUser) {
+      const preloadedState = { session: { currentUser: window.currentUser } };
+      store = configureStore(preloadedState);
+    } else {
+      store = configureStore();
+    }
+
     const root = document.getElementById('root');
-    window.signup = signup;
-    window.login = login;
-    window.logout = logout;
-    window.store = configureStore();
-    ReactDOM.render(<h1>Welcome to Photo Journal</h1>, root);
+    // window.store = store;
+    // window.login = login;
+    // window.logout = logout;
+    ReactDOM.render(<Root store={ store }/>, root);
 });
