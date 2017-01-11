4a70bbc179baf48a37f20796bfa1670062b3b27d
Mark Noizumi <marknoizumi@Marks-MacBook-Pro.local>
2017-01-10 21:14:59 -0800

    added photo and css styling to splash page

--- photo/frontend/components/app.jsx
+++ photo/frontend/components/app.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 
 const App = ({ children }) => (
-  <div>
-    <h1>Photo Journal</h1>
-    <GreetingContainer />
-    { children }
+  <div className="home">
+    <div className="header"><h1>Photo Journal</h1></div>
+    <div className="splash">
+      <GreetingContainer />
+      { children }
+    </div>
   </div>
 );
 
