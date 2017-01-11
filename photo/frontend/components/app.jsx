ea4f297523bbfe75d3096c2eb321e448673728fd
Mark Noizumi <mark.noizumi@gmail.com>
2017-01-11 15:03:57 -0800

    css styling for titles on splash page

--- photo/frontend/components/app.jsx
+++ photo/frontend/components/app.jsx
@@ -12,9 +12,12 @@ const App = ({ children }) => (
 
       <div className="titles">
         <br />
-        <h1> Tell your life story in pictures</h1>
-        <h2>Showcase your adventures, share your amazing photos, stay inspired!</h2>
+        <div className="title-text">
 
+          <h1> Tell your life story in pictures</h1>
+          <h2>Showcase your adventures, share your amazing photos, stay inspired!</h2>
+          <br/> <br/><br/> <br/>
+        </div>
       </div>
     </div>
   </div>
