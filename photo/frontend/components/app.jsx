832717192d8b42e92d37e19494c78c6616e46db8
Mark Noizumi <marknoizumi@Marks-MacBook-Pro.local>
2017-01-11 11:49:56 -0800

    Add css styling to buttons in header

--- photo/frontend/components/app.jsx
+++ photo/frontend/components/app.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import GreetingContainer from './greeting/greeting_container';
+import HeaderContainer from './header/header_container';
 
 const App = ({ children }) => (
   <div className="home">
     <div className="header">
       <img src="http://res.cloudinary.com/dseky3p5e/image/upload/c_scale,w_142/v1484119573/v8_c5rdfy.png" />
-      <GreetingContainer />
+      <HeaderContainer />
     </div>
     <div className="splash">
 
