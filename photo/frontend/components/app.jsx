52cdee066f4b775a851c2764a7b4ee617463d85b
Mark Noizumi <mark.noizumi@gmail.com>
2017-01-11 11:13:53 -0800

    Add login and logout buttons. Add titles to splash.

--- photo/frontend/components/app.jsx
+++ photo/frontend/components/app.jsx
@@ -3,10 +3,21 @@ import GreetingContainer from './greeting/greeting_container';
 
 const App = ({ children }) => (
   <div className="home">
-    <div className="header"><img src="http://res.cloudinary.com/dseky3p5e/image/upload/c_scale,w_210/v1484119573/v8_c5rdfy.png" /></div>
-    <div className="splash">
+    <div className="header">
+      <img src="http://res.cloudinary.com/dseky3p5e/image/upload/c_scale,w_142/v1484119573/v8_c5rdfy.png" />
       <GreetingContainer />
+    </div>
+    <div className="splash">
+
+
       { children }
+
+      <div className="titles">
+        <br />
+        <h1> Tell your life story in pictures</h1>
+        <h2>Showcase your adventures, share your amazing photos, stay inspired!</h2>
+
+      </div>
     </div>
   </div>
 );
