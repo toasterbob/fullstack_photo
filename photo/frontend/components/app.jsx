5813aa26c8a054ef03a07b990ee5d0955f5d6293
Mark Noizumi <mark.noizumi@gmail.com>
2017-01-10 23:28:48 -0800

    added logos and images

--- photo/frontend/components/app.jsx
+++ photo/frontend/components/app.jsx
@@ -3,7 +3,7 @@ import GreetingContainer from './greeting/greeting_container';
 
 const App = ({ children }) => (
   <div className="home">
-    <div className="header"><img src="http://res.cloudinary.com/dseky3p5e/image/upload/c_scale,w_186/v1484119092/v7_uvpirv.png" /></div>
+    <div className="header"><img src="http://res.cloudinary.com/dseky3p5e/image/upload/c_scale,w_210/v1484119573/v8_c5rdfy.png" /></div>
     <div className="splash">
       <GreetingContainer />
       { children }
