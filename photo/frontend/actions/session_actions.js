667a00e98a9705c96360b47da4c2950b581d117b
Mark Noizumi <mark.noizumi@gmail.com>
2017-01-12 15:54:16 -0800

    redirect for guest login and logout

--- photo/frontend/actions/session_actions.js
+++ photo/frontend/actions/session_actions.js
@@ -2,10 +2,14 @@ import * as APIUtil from '..//util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+import { hashHistory } from 'react-router';
 
 export const login = user => dispatch => (
   APIUtil.login(user)
-    .then(user => dispatch(receiveCurrentUser(user)),
+    .then(user => {
+      dispatch(receiveCurrentUser(user));
+      hashHistory.push("/profile");
+    },
       err => dispatch(receiveErrors(err.responseJSON)))
 );
 
@@ -16,7 +20,11 @@ export const signup = user => dispatch => (
 );
 
 export const logout = () => dispatch => (
-  APIUtil.logout().then(user => dispatch(receiveCurrentUser(null)))
+  APIUtil.logout().then(user => {
+    dispatch(receiveCurrentUser(null));
+    hashHistory.push("/");
+  }
+  )
 );
 
 export const receiveCurrentUser = currentUser => ({
