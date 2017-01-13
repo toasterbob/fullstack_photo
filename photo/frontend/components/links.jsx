5e7294449d9fa2080aa8a36224a92a2887d7ea83
Mark Noizumi <mark.noizumi@gmail.com>
2017-01-12 16:58:44 -0800

    profile page styling

---
+++ photo/frontend/components/links.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export const defaultCoverPic = "https://source.unsplash.com/2oFdVd00xOg/1600x500";
+export const defaultProfilePic = "http://res.cloudinary.com/dseky3p5e/image/upload/c_scale,w_110/v1484255960/profile_ans18c.png";
