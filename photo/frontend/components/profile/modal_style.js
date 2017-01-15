0a876e96d7a667f5cee92a627e4ac008ed41d51f
Mark Noizumi <mark.noizumi@gmail.com>
2017-01-14 23:41:26 -0800

    profile edit button opens modal

---
+++ photo/frontend/components/profile/modal_style.js
@@ -0,0 +1,24 @@
+const ModalStyle = {
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(255, 255, 255, 0.45)'
+  },
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : 'auto',
+    transform             : 'translate(-50%, -50%)',
+    borderRadius          : '3px',
+    border                : '1px solid grey',
+    padding               : '0px 20px',
+    backgroundColor       : '#f7f8fa'
+  }
+};
+
+export default ModalStyle;
