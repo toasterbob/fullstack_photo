89c18524fba9e769d7c71581f0d5485cdb282dee
Mark Noizumi <mark.noizumi@gmail.com>
2017-01-18 17:46:09 -0800

    create follows table

--- photo/db/schema.rb
+++ photo/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117005854) do
+ActiveRecord::Schema.define(version: 20170119013321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer  "following_id", null: false
+    t.integer  "follower_id",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.string   "photo_url",   null: false
