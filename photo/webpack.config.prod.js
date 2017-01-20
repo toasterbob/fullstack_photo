381d49761e93fdd084c4a8ad293316b65a13fd65
Mark Noizumi <mark.noizumi@gmail.com>
2017-01-19 17:31:35 -0800

    feed styling

---
+++ photo/webpack.config.prod.js
@@ -0,0 +1,25 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: './frontend/photo_journal.jsx',
+  output: {
+    path: 'app/assets/javascripts',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx' ]
+  }
+};
