296d56396a4beb656b6cd634c72c8f1073a7fd5c
Mark Noizumi <mark.noizumi@gmail.com>
2017-01-20 14:05:37 -0800

    journal mode styling

---
+++ photo/README.md
@@ -0,0 +1,4 @@
+![robinbanks-logo]
+[robinbanks-logo]: ./public/robinbanks.png
+[live][robinbanks]
+[robinbanks]: http://www.robinbanks.herokuapp.com
--- photo/frontend/components/session_form/session_form_container.js
+++ photo/frontend/components/session_form/session_form_container.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { login, logout, signup } from '../../actions/session_actions';
+import { login, logout, signup, receiveErrors } from '../../actions/session_actions';
 import SessionForm from './session_form';
 
 const mapStateToProps = ({ session }) => ({
@@ -15,7 +15,8 @@ const mapDispatchToProps = (dispatch, { location }) => {
   return {
     processForm: user => dispatch(processForm(user)),
     formType,
-    guestUser: user => dispatch(login({username: "guest", password: "password"}))
+    guestUser: user => dispatch(login({username: "guest", password: "password"})),
+    receiveErrors: () => dispatch(receiveErrors([]))
   };
 };
 
